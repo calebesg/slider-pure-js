fde873335c894dc71f4e93cd87829feb7828717c
CalebeSG <calebeguimaraes10@gmail.com>
2022-04-08 16:31:03 -0300

    remove useless files

---
+++ script.js
@@ -0,0 +1,20 @@
+'use strict';
+
+const sliderItems = [
+  {
+    name: 'Tanya Sinclair',
+    job: 'UX Engineer',
+    bio: `“ I’ve been interested in coding for a while but never taken the
+    jump, until now. I couldn’t recommend this course enough. I’m now
+    in the job of my dreams and so excited about the future. ”`,
+    avatar: 'image-tanya.jpg',
+  },
+  {
+    name: 'John Tarkpor',
+    job: 'Junior Front-end Developer',
+    bio: `“ If you want to lay the best foundation possible I’d recommend taking this course. 
+    The depth the instructors go into is incredible. I now feel so confident about 
+    starting up as a professional developer. ”`,
+    avatar: 'image-tanya.jpg',
+  },
+];
